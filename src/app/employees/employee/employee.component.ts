6bee04cb9bc078f6f72fb57601afb90ac4424a81
Anuj Jain <lookforaj@gmail.com>
2018-06-22 18:11:44 +0530

    Changes in employee component
    Added form validation, reset function and styling.

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import {EmployeeService } from '../shared/employee.service';
 @Component({
   selector: 'app-employee',
@@ -10,6 +11,23 @@ export class EmployeeComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  onSubmit(employeeForm: NgForm) {
+
+  }
+
+  resetForm(employeeForm?: NgForm) {
+    if(employeeForm!= null) {
+      employeeForm.reset();
+      this.employeeService.selectedEmployee = {
+        $key: null,
+        name: '',
+        position: '',
+        office: '',
+        salary: 0
+      }
+    }
+  }
 }
