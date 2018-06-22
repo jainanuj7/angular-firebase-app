4cdde04e42b02466658d85006bdfc225ea5879ff
Anuj Jain <lookforaj@gmail.com>
2018-06-22 17:11:00 +0530

    First Commit
    Basic structure of pp designed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
-
+import {environment } from '../environments/environment';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAc3dGy8avH80PdkXYfyFqRT3gUP5wtU9E",
+    authDomain: "angular-firebase-app-e961c.firebaseapp.com",
+    databaseURL: "https://angular-firebase-app-e961c.firebaseio.com",
+    projectId: "angular-firebase-app-e961c",
+    storageBucket: "angular-firebase-app-e961c.appspot.com",
+    messagingSenderId: "429944583515"
+  }
 };
