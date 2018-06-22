42e4ff2ebf87c6822289e4a78b95a4da5adabf2b
Anuj Jain <lookforaj@gmail.com>
2018-06-22 22:48:21 +0530

    Added firebaseConfig to environment.production.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAc3dGy8avH80PdkXYfyFqRT3gUP5wtU9E",
+    authDomain: "angular-firebase-app-e961c.firebaseapp.com",
+    databaseURL: "https://angular-firebase-app-e961c.firebaseio.com",
+    projectId: "angular-firebase-app-e961c",
+    storageBucket: "angular-firebase-app-e961c.appspot.com",
+    messagingSenderId: "429944583515"
+  }
 };
